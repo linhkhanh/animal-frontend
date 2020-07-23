0d47fa80ef6fbf29ac92276d8f208d73d3162dd5
linhle <linhle@Hieus-MacBook-Air.local>
2020-07-23 15:41:36 +0800

    Updates

---
+++ precache-manifest.bf3055c89683652f7c3afd459d8c73b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af725a4bf8d6ffc8c237147bfecab7ba",
+    "url": "/animal-frontend/index.html"
+  },
+  {
+    "revision": "fa88cb9027f6a6ea6b4b",
+    "url": "/animal-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f84e598c9555f9e9d700",
+    "url": "/animal-frontend/static/js/2.778e784b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animal-frontend/static/js/2.778e784b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa88cb9027f6a6ea6b4b",
+    "url": "/animal-frontend/static/js/main.b60ecf48.chunk.js"
+  },
+  {
+    "revision": "4643b5df65734104f5bd",
+    "url": "/animal-frontend/static/js/runtime-main.30fa0c58.js"
+  }
+]);
